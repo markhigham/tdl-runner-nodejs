fe7619b1b28a66e0ef1506e9dc4aefab478138b9
neomatrix369 <sadhak001@gmail.com>
2018-06-02 22:23:05 +0100

    Amending the coverage script to run command to invoke coverage tool and extract the coverage info

--- get_coverage_for_challenge.sh
+++ get_coverage_for_challenge.sh
@@ -8,17 +8,18 @@ set -o pipefail
 SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 CHALLENGE_ID=$1
-NODEJS_TEST_REPORT_CSV_FILE="${SCRIPT_CURRENT_DIR}/coverage/results.csv"
+NODEJS_TEST_REPORT_JSON_FILE="${SCRIPT_CURRENT_DIR}/coverage/coverage-summary.json"
 NODEJS_CODE_COVERAGE_INFO="${SCRIPT_CURRENT_DIR}/coverage.tdl"
 
-( cd ${SCRIPT_CURRENT_DIR} && npm install && npm test || true 1>&2 )
+( cd ${SCRIPT_CURRENT_DIR} && npm install && npm run coverage || true 1>&2 )
 
 [ -e ${NODEJS_CODE_COVERAGE_INFO} ] && rm ${NODEJS_CODE_COVERAGE_INFO}
 
-if [ -f "${NODEJS_TEST_REPORT_CSV_FILE}" ]; then
-    COVERAGE_OUTPUT=$(grep ${CHALLENGE_ID} ${NODEJS_TEST_REPORT_CSV_FILE} | tr ',' ' ')
-    MISSED=$(echo $COVERAGE_OUTPUT | awk '{print $6}')
-    COVERED=$(echo $COVERAGE_OUTPUT | awk '{print $5}')
+if [ -f "${NODEJS_TEST_REPORT_JSON_FILE}" ]; then
+    prettier --write ${NODEJS_TEST_REPORT_JSON_FILE}
+    COVERAGE_OUTPUT=$(grep ${CHALLENGE_ID} ${NODEJS_TEST_REPORT_JSON_FILE} -A 4 | grep -o -P '(?<="lines": {).*(?=},)' | tr -d ' "')
+    MISSED=$(echo $COVERAGE_OUTPUT | awk -F "," '{print $3}' | awk -F ":" '{print $2}')
+    COVERED=$(echo $COVERAGE_OUTPUT | awk -F "," '{print $2}' | awk -F ":" '{print $2}')
     TOTAL_LINES=$((MISSED + $COVERED))
     echo $(($COVERED * 100 / $TOTAL_LINES)) > ${NODEJS_CODE_COVERAGE_INFO}
     cat ${NODEJS_CODE_COVERAGE_INFO}
